14b11fc0544580b05028bb320cf26fb8d32ff6f5
alessia263456 <alessiamihaila05@gmail.com>
2026-03-17 09:04:34 +0200

    Tema Laborator 2

---
+++ src/ro/ulbs/proiectaresoftware/students/Application.java
@@ -0,0 +1,76 @@
+package ro.ulbs.proiectaresoftware.students;
+
+import java.util.*;
+
+public class Application {
+    boolean gaseste(List<Student> lista, Student s)
+    {
+        for(Student st: lista)
+        {
+            if(Objects.equals(st.getNumarMatricol(), s.getNumarMatricol()) &&
+                    Objects.equals(st.getPrenume(), s.getPrenume()) &&
+                    Objects.equals(st.getNume(), s.getNume()) &&
+                    Objects.equals(st.getFormatieDeStudiu(), s.getFormatieDeStudiu()) )
+                return true;
+        }
+        return false;
+    }
+    boolean gaseste(Set<Student> set, Student s)
+    {
+        return set.contains(s);
+    }
+    public void main()
+    {
+        Student s1 = new Student(112, "Ioan", "Popa", "TI21/1");
+        Student s2 = new Student(112, "Maria", "Oprea", "TI21/1");
+        Student s3 = new Student(120, "Alis", "Popa", "TI21/2");
+        Student s4 = new Student(122, "Mihai", "Vecerdea", "TI22/1");
+        Student s5 = new Student(122, "Eugen", "Uritescu", "TI22/2");
+
+        System.out.printf("%15s %15s %15s %15s\n",
+                "numar matricol", "prenume", "nume", "formatie de studiu");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+
+        List<Student> lista = new ArrayList<Student>();
+        lista.add(s1);
+        lista.add(s2);
+        lista.add(s3);
+        lista.add(s4);
+        lista.add(s5);
+        System.out.println("lista:");
+        for(Student s: lista)
+            System.out.println(s);
+
+        //tema casa
+        Set<Student> set_studenti = new HashSet<>(lista);
+
+        System.out.println("//lista");
+        Student s6=new Student(120, "Alis", "Popa", "TI21/2");
+        if(gaseste(lista,s6))
+            System.out.println(s6 + " este in lista");
+        else
+            System.out.println(s6 + " nu este in lista");
+        Student s7=new Student(112, "Maria", "Popa", "TI21/1");
+        if(gaseste(lista,s7))
+            System.out.println(s7 + " este in lista");
+        else
+            System.out.println(s7 + " nu este in lista");
+
+        System.out.println("//set");
+        if(gaseste(set_studenti,s6))
+            System.out.println(s6 + " este in lista");
+        else
+            System.out.println(s6 + " nu este in lista");
+
+
+        if(gaseste(set_studenti,s7))
+            System.out.println(s7 + " este in lista");
+        else
+            System.out.println(s7 + " nu este in lista");
+
+    }
+}
---
+++ src/ro/ulbs/proiectaresoftware/students/Student.java
@@ -0,0 +1,57 @@
+package ro.ulbs.proiectaresoftware.students;
+
+import java.util.Objects;
+
+public class Student
+{
+    private Integer numarMatricol;
+    private String prenume, nume, formatieDeStudiu;
+
+    Student(Integer numarMatricol, String prenume, String nume, String formatieDeStudiu)
+    {
+        this.numarMatricol=numarMatricol;
+        this.prenume=prenume;
+        this.nume=nume;
+        this.formatieDeStudiu=formatieDeStudiu;
+    }
+
+    public Integer getNumarMatricol() {
+        return numarMatricol;
+    }
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public String getFormatieDeStudiu() {
+        return formatieDeStudiu;
+    }
+
+
+
+    @Override
+    public String toString()
+    {
+        return String.format("%15d %15s %15s %15s",
+                numarMatricol, prenume, nume, formatieDeStudiu);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Student)) return false;
+            Student s = (Student) o;
+            return Objects.equals(numarMatricol, s.numarMatricol) &&
+                    Objects.equals(prenume, s.prenume) &&
+                    Objects.equals(nume, s.nume) &&
+                    Objects.equals(formatieDeStudiu, s.formatieDeStudiu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numarMatricol, prenume, nume, formatieDeStudiu);
+    }
+}
